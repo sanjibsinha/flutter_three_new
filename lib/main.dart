76b0e691fb49a10c11a9ff5fcfbc6684585a0ad6
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-12-30 06:55:23 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,77 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: unnecessary_new
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      title: 'Lazy Load Demo',
+      home: new MyHomePage(title: 'Lazy Load Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<int> verticalData = [];
+  List<int> horizontalData = [];
 
-  void _incrementCounter() {
+  final int increment = 10;
+
+  bool isLoadingVertical = false;
+  bool isLoadingHorizontal = false;
+
+  @override
+  void initState() {
+    _loadMoreVertical();
+    _loadMoreHorizontal();
+    super.initState();
+  }
+
+  Future _loadMoreVertical() async {
     setState(() {
-      _counter++;
+      isLoadingVertical = true;
+    });
+
+    // Add in an artificial delay
+    await new Future.delayed(const Duration(seconds: 2));
+
+    verticalData.addAll(
+        List.generate(increment, (index) => verticalData.length + index));
+
+    setState(() {
+      isLoadingVertical = false;
+    });
+  }
+
+  Future _loadMoreHorizontal() async {
+    setState(() {
+      isLoadingHorizontal = true;
+    });
+
+    // Add in an artificial delay
+    await new Future.delayed(const Duration(seconds: 2));
+
+    horizontalData.addAll(
+        List.generate(increment, (index) => horizontalData.length + index));
+
+    setState(() {
+      isLoadingHorizontal = false;
     });
   }
 
@@ -43,26 +81,91 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: LazyLoadScrollView(
+        isLoading: isLoadingVertical,
+        onEndOfPage: () => _loadMoreVertical(),
+        child: Scrollbar(
+          child: ListView(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Nested horizontal ListView',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                  height: 180,
+                  child: LazyLoadScrollView(
+                      isLoading: isLoadingHorizontal,
+                      scrollDirection: Axis.horizontal,
+                      onEndOfPage: () => _loadMoreHorizontal(),
+                      child: Scrollbar(
+                          child: ListView.builder(
+                              itemCount: horizontalData.length,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, position) {
+                                return DemoItem(position);
+                              })))),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Vertical ListView',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: verticalData.length,
+                itemBuilder: (context, position) {
+                  return DemoItem(position);
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
 }
-// adding new
+
+class DemoItem extends StatelessWidget {
+  final int position;
+
+  const DemoItem(
+    this.position, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    color: Colors.grey,
+                    height: 40.0,
+                    width: 40.0,
+                  ),
+                  const SizedBox(width: 8.0),
+                  Text("Item $position"),
+                ],
+              ),
+              const Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed vulputate orci. Proin id scelerisque velit. Fusce at ligula ligula. Donec fringilla sapien odio, et faucibus tortor finibus sed. Aenean rutrum ipsum in sagittis auctor. Pellentesque mattis luctus consequat. Sed eget sapien ut nibh rhoncus cursus. Donec eget nisl aliquam, ornare sapien sit amet, lacinia quam."),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
