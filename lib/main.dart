1599d2f34202f95057f7bc5555733bf6689d100a
Sanjib Sinha <sanjib12sinha@gmail.com>
2023-01-03 06:21:16 +0530

    first in sliver

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_three_new/main_sliver.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,16 +31,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  onButtonTap(Widget page) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (BuildContext context) => page));
   }
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -50,19 +51,37 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            MyMenuButton(
+              title: "Custom AppBar & SliverAppBar",
+              actionTap: () {
+                onButtonTap(const MainAppBarSliverAppBar());
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+// adding new
+class MyMenuButton extends StatelessWidget {
+  final String? title;
+  final VoidCallback? actionTap;
+
+  const MyMenuButton({super.key, this.title, this.actionTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: MaterialButton(
+        height: 50.0,
+        color: Theme.of(context).primaryColor,
+        textColor: Colors.white,
+        onPressed: actionTap,
+        child: Text(title!),
       ),
     );
   }
 }
-// adding new
